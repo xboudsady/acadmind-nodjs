31a8e70a0d54621e2324ccacf281e952da5f5728
xboudsady <boudsadyx@gmail.com>
2019-06-21 14:16:16 +0200

    Rendering html

---
+++ 03-render-html/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+function onRequest(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  fs.readFile('./index.html', null, function(error, data){
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found.');
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
+}
+
+http.createServer(onRequest).listen(8000);
