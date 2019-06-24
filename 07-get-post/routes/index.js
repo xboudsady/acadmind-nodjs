3d83ecc684541f6f245d7284f1565580f9ece9a5
xboudsady <boudsadyx@gmail.com>
2019-06-24 13:24:13 +0200

    Completed project 07-get-post

--- 07-get-post/routes/index.js
+++ 07-get-post/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cool, huh?', condition: true,  anyArray: [1,2,3] });
 });
 
+router.get('/test/:id', function(req, res, next) {
+  res.render('test', { output: req.params.id });
+});
+
+router.post('/test/submit', function(req, res, next) {
+  var id = req.body.id;
+  res.redirect('/test/' + id);
+});
+
 module.exports = router;
